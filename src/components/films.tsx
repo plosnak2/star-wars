037255b39ea85010c469469b9441b81a650515b8
plosnak2 <jakub.zaukolec@gmail.com>
2023-06-24 12:13:33 +0200

    responsive design for movie page

--- src/components/films.tsx
+++ src/components/films.tsx
@@ -12,8 +12,8 @@ export const Films: FC<IProps> = ({films}) => {
             films.map((movie, index) => (
                 <div className="movie">
                     <div className="movie-img-info">
-                        <div>
-                            <img src="https://placehold.co/350x200"/>
+                        <div className="movie-img">
+                            <img src="https://placehold.co/350x200" width={350} height={200}/>
                         </div>
                         <div className="movie-info">
                             <h3>{movie.title} - {movie.release_date.toString()}</h3>
